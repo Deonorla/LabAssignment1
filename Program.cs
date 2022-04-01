59e290e0e686541c17dc1dd1b273ec1b6ae58a7f
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2022-04-01 22:04:48 +0100

    Lab__Assignment1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace backend1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Emmanuel, I am in backend of Wella Schools");
+            
+            Console.WriteLine("I want to learn backend development because i want to know how operations, datas and APIs functions in the background, also i want to be able to become a fullstack developer and land my dream job as a software developer.");
+        }
+    }
+}
